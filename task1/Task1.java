2e92914ad1e06a502665ea39535466b9654747f7
Anleon1991 <anleon1991@tut.by>
2022-04-02 09:58:05 +0300

    correct

--- task1/Task1.java
+++ task1/Task1.java
@@ -1,10 +1,12 @@
+package task1;
+
 import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Введите строку:");
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        char[] symbol = str.toCharArray();
+        String inputLine = scanner.nextLine();
+        char[] symbol = inputLine .toCharArray();
         int summa = 0;
         for (int i = 0; i < symbol.length; i++) {
             if (symbol[i] <= '9' && symbol[i] >= '1') {
--- task2/Task2.java
+++ task2/Task2.java
@@ -1,22 +1,24 @@
+package task2;
+
 import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
-        int x = 0;
+        int inputNumber = 0;
         while (true){
             System.out.println("Введите целое число:");
             Scanner scanner = new Scanner(System.in);
             if (scanner.hasNextInt()) {
-                x = scanner.nextInt();
+                inputNumber = scanner.nextInt();
                 break;
             } else {
                 System.out.println("Вы ввели не целое число!");
             }
         }
-        for (int d = 2; x > 1; d++) {
-            if (x % d == 0) {
-                x = x / d;
-                for (int i = 0; x % d == 0; i++) {
-                    x = x / d;
+        for (int d = 2; inputNumber > 1; d++) {
+            if (inputNumber % d == 0) {
+                inputNumber = inputNumber / d;
+                for (int i = 0; inputNumber % d == 0; i++) {
+                    inputNumber = inputNumber / d;
                 }
                 System.out.println(d);
             }
